be34a0a7db62b238aa96915772a73a76f8726f6b
Jordan Henderson <jordan.henderson@ioflame.com>
2013-09-26 22:51:14 +1000

    Moved nginx boot into webapp project. Created blank response module.

--- gallery/src/ngx_modules.c
+++ gallery/src/ngx_modules.c
@@ -49,6 +49,7 @@ extern ngx_module_t  ngx_http_headers_filter_module;
 extern ngx_module_t  ngx_http_copy_filter_module;
 extern ngx_module_t  ngx_http_range_body_filter_module;
 extern ngx_module_t  ngx_http_not_modified_filter_module;
+extern ngx_module_t  ngx_http_hello_module;
 
 ngx_module_t *ngx_modules[] = {
     &ngx_core_module,
@@ -96,6 +97,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_copy_filter_module,
     &ngx_http_range_body_filter_module,
     &ngx_http_not_modified_filter_module,
+	&ngx_http_hello_module,
     NULL
 };
 
